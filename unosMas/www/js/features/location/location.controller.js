e6206f185f024ae7dad7df9aa395ac763279461a
jsalamanca11 <j.salamanca611@gmail.com>
2017-03-23 14:24:55 -0400

    map code in controller

--- unosMas/www/js/features/location/location.controller.js
+++ unosMas/www/js/features/location/location.controller.js
@@ -2,12 +2,29 @@ angular
 	.module('unoMas.location')
 	.controller('LocationCtrl', LocationCtrl);
 
-function LocationCtrl($state) {
+function LocationCtrl($scope, $state, $cordovaGeolocation) {
 	var vm = this;
 
 	vm.continue = function(){
 		$state.go('selection');
 	};
 
+	var options = {timeout: 10000, enableHighAccuracy: true};
+ 
+  	$cordovaGeolocation.getCurrentPosition(options).then(function(position){
+ 
+    	var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+ 
+	    var mapOptions = {
+	      center: latLng,
+	      zoom: 15,
+	      mapTypeId: google.maps.MapTypeId.ROADMAP
+	    };
+ 
+    	$scope.map = new google.maps.Map(document.getElementById("location"), mapOptions);
+ 
+	}, function(error){
+	    console.log("Could not get location");
+	});
 
 }
--- unosMas/www/js/features/location/location.route.js
+++ unosMas/www/js/features/location/location.route.js
@@ -2,12 +2,13 @@ angular
 	.module('unoMas.location')
 	.config(locationConfig);
 
-function locationConfig($stateProvider) {
+function locationConfig($stateProvider, $urlRouterProvider) {
 	$stateProvider.state({
 		name: 'location',
 		url: '/location',
 		templateUrl: 'js/features/location/location.html',
 		controller: 'LocationCtrl',
 		controllerAs: 'LocationVM'
-	})
+	});
+	$urlRouterProvider.otherwise("/");
 }
